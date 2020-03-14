57db539c9cf1b36fecd1cc27383d2175c2276ad2
Steven <steven.gu@letote.cn>
2020-03-14 19:59:19 +0800

    Implements simple file upload. (#20)

--- examples/upload_file.rs
+++ examples/upload_file.rs
@@ -1,12 +1,27 @@
-use qiniu_upload_rs::upload::file_uploader::FileUploader;
+use qiniu_upload_rs::{upload::file_uploader::FileUploader, utils::auth::Auth};
+
+const ACCESS_KEY: &str = "test-access-key";
+const SECRET_KEY: &str = "test-secret-key";
+
+const BUCKET: &str = "test-bucket";
+const KEY: &str = "test-file-key.txt";
+const EXPIRED_SECONDS: u64 = 3600;
+
+const FILE_PATH: &str = "examples/fixtures/simple.txt";
 
 fn main() {
     println!("Starts to upload a file for an example");
 
-    let file_path = "examples/data/simple.txt";
+    let auth = Auth {
+        access_key: ACCESS_KEY.to_string(),
+        secret_key: SECRET_KEY.to_string(),
+    };
 
-    let uploader = FileUploader::new();
-    uploader.upload("dummy_upload_file", file_path).unwrap();
+    let uploader = FileUploader::new(auth);
+
+    uploader
+        .upload(BUCKET, KEY, EXPIRED_SECONDS, FILE_PATH)
+        .unwrap();
 
     println!("Finishes file upload");
 }
--- src/upload/file_uploader.rs
+++ src/upload/file_uploader.rs
@@ -1,13 +1,34 @@
-use crate::utils::qiniu_err::QiniuErr;
+use crate::upload::{build_form, upload_form};
+use crate::utils::{auth::Auth, qiniu_err::QiniuErr};
 
-pub struct FileUploader {}
+use std::{
+    fs::File,
+    io::{BufReader, Read},
+    sync::Arc,
+};
+
+pub struct FileUploader {
+    auth: Auth,
+}
 
 impl FileUploader {
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(auth: Auth) -> Self {
+        Self { auth }
     }
 
-    pub fn upload(&self, _key: &str, _file_path: &str) -> Result<(), QiniuErr> {
-        Ok(())
+    pub fn upload(
+        &self,
+        bucket: &str,
+        key: &str,
+        expired_seconds: u64,
+        file_path: &str,
+    ) -> Result<(), QiniuErr> {
+        let file = File::open(file_path)?;
+        let mut reader = BufReader::new(file);
+        let mut data = Vec::new();
+        reader.read_to_end(&mut data)?;
+
+        let form = build_form(&self.auth, bucket, key, expired_seconds, Arc::new(data))?;
+        upload_form(form)
     }
 }
